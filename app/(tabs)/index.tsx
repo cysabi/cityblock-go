ce9bd7f5f3f06c2cd29231169ea3f334b97cf34b
Claire Kwong <clairebkwong@gmail.com>
2026-03-22 16:08:04 -0400

    display map even for dummy location

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,19 @@
-import { View, ActivityIndicator, Text } from "react-native";
-import MapView, { Marker } from "react-native-maps";
 import { useBackgroundLocation } from "@/hooks/use-location";
+import { ActivityIndicator, Text, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
 export default function HomeScreen() {
   const { location, status, error } = useBackgroundLocation();
 
   if (status === "starting") return <ActivityIndicator style={{ flex: 1 }} />;
   if (status === "error") return <Text>{error}</Text>;
-  if (!location) return null;
 
-  const { latitude, longitude } = location.coords;
+  const { latitude, longitude } = location
+    ? location.coords
+    : { latitude: 40.7128, longitude: 74.006 };
 
   return (
-    <View style={{ flex: 1 }}>
+    <View>
       <MapView
         style={{
           width: "100%",
@@ -27,7 +28,7 @@ export default function HomeScreen() {
       >
         <Marker
           coordinate={{ latitude, longitude }}
-          title={`last updated ${location.timestamp}`}
+          title={`last updated ${location ? location.timestamp : 0}`}
         />
       </MapView>
     </View>
